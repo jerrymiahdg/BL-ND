9d3f4f75f861ab9be9dcb6ebf62caa547080e5fd
jerrymiahdg <124826126+jerrymiahdg@users.noreply.github.com>
2023-03-25 14:53:45 -0700

    Update blind.js

--- blind.js
+++ blind.js
@@ -2,10 +2,13 @@ const home = document.querySelector('.home')
 const ourMission = document.querySelector('.our-mission')
 const ourTeam = document.querySelector('.our-team')
 const worksCited = document.querySelector('.works-cited')
+const ourWork = document.querySelector('.our-work')
 const ourMissionBtn = document.querySelector('#our-mission-btn')
 const ourTeamBtn = document.querySelector('#our-team-btn')
 const homeBtn = document.querySelector('.title-btn')
 const worksCitedBtn = document.querySelector('#works-cited-btn')
+const ourWorkBtn = document.querySelector('#our-work-btn')
+const checkBtn = document.querySelector('.check-btn')
 let currentPage = '.home'
 homeBtn.addEventListener('click', function() {
     document.querySelector(currentPage).style.display = 'none'
@@ -26,4 +29,14 @@ worksCitedBtn.addEventListener('click', function() {
     document.querySelector(currentPage).style.display = 'none'
     worksCited.style.display = 'block'
     currentPage = '.works-cited'
+})
+ourWorkBtn.addEventListener('click', function() {
+    document.querySelector(currentPage).style.display = 'none'
+    ourWork.style.display = 'block'
+    currentPage = '.our-work'
+})
+checkBtn.addEventListener('click', function() {
+    document.querySelector(currentPage).style.display = 'none'
+    ourWork.style.display = 'block'
+    currentPage = '.our-work'
 })
